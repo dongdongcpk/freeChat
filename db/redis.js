4e1285d594eac513d9661e595ed9124fe281ec15
dongdonggun <138856015@qq.com>
2015-06-04 11:20:00 +0800

    change redis.js & player.js

--- db/redis.js
+++ db/redis.js
@@ -1,6 +1,10 @@
 var redis = require('redis');
 var client = redis.createClient();
 
+var sub = function() {
+    return redis.createClient();
+};
+
 client.on('connect', function() {
     console.log('connect redis success !');
 });
@@ -9,12 +13,7 @@ client.on('error', function() {
     console.log('connect redis error !');
 });
 
-client.on('subscribe', function(channel, count) {
-    console.log(channel, count);
-});
-
-client.on('message', function(channel, message) {
-    console.log('channel ' + channel + ': ' + message);
-});
-
-module.exports = client;
+module.exports = {
+    client: client,
+    sub: sub
+};
--- domain/player.js
+++ domain/player.js
@@ -1,15 +1,24 @@
-//var uuid = require('node-uuid');
 var redis = require('../db/redis');
+var client = redis.client;
 var C = require('../const/const');
 
 var Player = function() {
     this.uid = '001';
-    //this.init();
+    this.channel = {};
+    this.init();
 };
 
 Player.prototype.init = function() {
-    this.uid = uuid.v1();
+    this.sub = redis.sub();
 };
+
+Player.sub.on('subscribe', function(channel, count) {
+    console.log('subscribe: ', channel, count);
+});
+
+Player.sub.on('message', function(channel, message) {
+    console.log('channel: ' + channel + ': ' + message);
+});
 
 /**
  * 玩家上线
@@ -18,7 +27,10 @@ Player.prototype.online = function() {
     this.addChannel(C.CHANNEL.GLOBAL);
     var fUids = this.getAllFriends();
     for(var i = 0, len = fUids.length; i < len; i++) {
-        this.addChannel(fUids[i]);
+        var channelTo = this.uid + ':' + fUids[i]; // as follow: '001:002'
+        var channelFrom = fUids[i] + ':' + this.uid; // as follow: '002:001'
+        this.bindChannel(channelTo, fUids[i]);
+        this.addChannel(channelFrom);
     }
 };
 
@@ -34,15 +46,21 @@ Player.prototype.offline = function() {
  * @returns {string[]}
  */
 Player.prototype.getAllFriends = function() {
-    return ['002', '003', '004', '005'];
+    //todo return real friends
+    if(this.uid === '001') {
+        return ['002', '003', '004', '005'];
+    }
+    if(this.uid === '002') {
+        return ['001', '003', '004', '005'];
+    }
 };
 
 /**
  * 订阅频道
  */
 Player.prototype.addChannel = function(channel) {
-    if(channel) {
-        redis.subscribe(channel);
+    if(channel && !!this.sub) {
+        this.sub.subscribe(channel);
     }
 };
 
@@ -50,8 +68,30 @@ Player.prototype.addChannel = function(channel) {
  * 取消频道
  */
 Player.prototype.cancelChannel = function(channel) {
-    if(channel) {
-        redis.unsubscribe(channel);
+    if(channel && !!this.sub) {
+        this.sub.unsubscribe(channel);
+    }
+};
+
+Player.prototype.bindChannel = function(channel, uid) {
+    this.channel[uid] = channel;
+};
+
+/**
+ * 发送消息
+ */
+Player.prototype.sendMsg = function(uid, msg) {
+    if(!!msg) {
+        var channel = this.channel[uid];
+        if(channel) {
+            client.publish(channel, msg);
+        }
+    }
+};
+
+Player.prototype.sendGlobalMsg = function(msg) {
+    if(!!msg) {
+        client.publish(C.CHANNEL.GLOBAL, msg);
     }
 };
 
